77189867615fa0890b55ef0f0091bbf42587a3fa
WillRenan <willdesign.will@gmail.com>
2021-07-28 22:17:59 -0300

    adicionando popup toggle

--- js/menu.js
+++ js/menu.js
@@ -4,8 +4,9 @@ var menu = document.querySelector('.header');
 window.addEventListener('scroll',()=>{
 
  if(window.scrollY > 10 ){
-  menu.classList.remove('scrollZero'); 
   menu.classList.add('scrollOne');
+  menu.classList.remove('scrollZero'); 
+  
    
   
  } else{
